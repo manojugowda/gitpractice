d8c84133fdec5a0877680e78ebc655b90819d3cc
manojugowda <manoj.u46@gmail.com>
2021-12-10 17:28:49 +0000

    first commit

---
+++ file.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "enter the pattern in the file"
+read pattern
+grep -R -l $pattern * > output
+if [ $? -eq 0 ]
+then
+	echo "the below files contain a pattern"
+	cat output
+else 
+	echo"the file doesnot contain a pattern"
+fi
